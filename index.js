10e3de735d6083d05cd79ce4155995dd158f3c42
Rina Yasnovskaya <rina.yasnovskaya@gmail.com>
2021-06-12 20:51:06 +0300

    feat: add views & index.js $ some packages

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-console.log();
+const express = require('express');
+const path = require('path');
+const PORT = 8080;
+
+const app = express();
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('views engine', 'ejs');
+
+app.use(express.static(path.join(__dirname, 'build')));
+app.engine('html', require('ejs').renderFile);
+
+app.get('/', (req, res) => {
+  res.render('index.html', { title: 'home page' });
+});
+
+app.listen(PORT, () => {
+  console.log('server has been started');
+});

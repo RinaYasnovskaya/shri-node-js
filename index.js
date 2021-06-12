4c04da067772640712e5ccaacd79b917cffab16b
Rina Yasnovskaya <rina.yasnovskaya@gmail.com>
2021-06-12 21:40:06 +0300

    feat: change index.js & add editorconfig & eslintrc & prettierrc

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const path = require('path');
+
 const PORT = 8080;
 
 const app = express();
@@ -8,7 +9,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('views engine', 'ejs');
 
 app.use(express.static(path.join(__dirname, 'build')));
-app.engine('html', require('ejs').renderFile);
+// app.engine('html', require('ejs').renderFile);
 
 app.get('/', (req, res) => {
   res.render('index.html', { title: 'home page' });

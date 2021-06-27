9efaddf7d993044bb8808100db62564669a96c4c
Rina Yasnovskaya <rina.yasnovskaya@gmail.com>
2021-06-27 15:14:03 +0300

    feat: add validate for form

--- client/src/components/settings/Settings.jsx
+++ client/src/components/settings/Settings.jsx
@@ -6,8 +6,36 @@ import { Field, reduxForm } from 'redux-form'
 import './settings.scss';
 import { postSettingsAsync } from '../../actions';
 
+const validate = values => {
+  const errors = {};
+  if (!values.repoName) {
+    errors.repoName = 'Required';
+  } else if (!values.buildCommand) {
+    errors.buildCommand = 'Required';
+  } else if (+values.time < 0) {
+    errors.time = 'err';
+  }
+  return errors;
+}
+
+const renderField = ({
+  input,
+  placeholder,
+  type,
+  id,
+  name,
+  meta: { touched, error }
+}) => {
+  const styles = (touched && error) ? {borderColor: 'red'} : {};
+
+  return (
+    <input {...input} style={styles} id={id} name={name} placeholder={placeholder} type={type} />
+  )
+}
+
 const SettingsForm = () => {
   const [disabled, setDisabled] = useState(false);
+  const [isFullData, setIsFullData] = useState(true);
 
   const dispatch = useDispatch();
   const { settings } = useSelector((state) => state.form);
@@ -34,6 +62,11 @@ const SettingsForm = () => {
         setDisabled(true);
         dispatch(postSettingsAsync(result, setDisabled));
       }
+    } else {
+      setIsFullData(false);
+      setTimeout(() => {
+        setIsFullData(true);
+      }, 2000);
     }
   };
 
@@ -49,27 +82,29 @@ const SettingsForm = () => {
         <label htmlFor="repo-name">
           GitHub repository <span className="red-star">*</span>
         </label>
-        <Field required={true} name="repoName" component="input" type="text" label="user-name/repo-name" />
+        <Field name="repoName" component={renderField} type="text" placeholder="user-name/repo-name" />
         <label id="build_command">
           Build Command <span className="red-star">*</span>
         </label>
-        <Field required name="buildCommand" id="build-command" component="input" type="text" label="build command: npm start" />
+        <Field name="buildCommand" id="build-command" component={renderField} type="text" placeholder="build command: npm start" />
         <label htmlFor="main-branch">Main Branch</label>
-        <Field name="mainBranch" id="main-branch" component="input" type="text" label="main branch: main" />
+        <Field name="mainBranch" id="main-branch" component={renderField} type="text" placeholder="main branch: main" />
         <label htmlFor="time">
-          Synchronize every <Field name="time" id="time" component="input" min="0" type="number" /> minutes
+          Synchronize every <Field name="time" id="time" component={renderField} min="0" type="number" /> minutes
         </label>
       </div>
         <button disabled={disabled} className="button button_bright submit" onClick={submitForm}>Save</button>
         <button disabled={disabled} className="button button_light cancel" onClick={onClickCancel}>
           <Link to="/">Cancel</Link>
         </button>
-        { (settingsIsDone === 'error') && <span className="modal-error">Error to save settings</span>}
-        { (settingsIsDone === 'done') &&  <span className="modal-success">Success</span>}
+        { (settingsIsDone === 'error') && <span className="modal-error">Error to save settings</span> }
+        { (settingsIsDone === 'done') &&  <span className="modal-success">Success</span> }
+        { !isFullData && <span className="modal-error">Required fields are empty</span> }
     </form>
   );
 };
 
 export const Settings = reduxForm({
   form: 'settings',
+  validate
 })(SettingsForm);

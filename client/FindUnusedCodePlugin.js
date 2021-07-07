e1560006db1af24956f0ffd51b9fa1cd2c8159fe
Rina Yasnovskaya <rina.yasnovskaya@gmail.com>
2021-07-08 00:58:55 +0300

    feat: add search all files on folders

--- client/FindUnusedCodePlugin.js
+++ client/FindUnusedCodePlugin.js
@@ -1,11 +1,22 @@
+const fs = require('fs');
+const path = require('path');
 class FindUnusedCodePlugin {
   apply (compiler) {
-    compiler.hooks.done.tap('Find Unused Code Plugin', (
+    compiler.hooks.afterCompile.tap('Find Unused Code Plugin', (
     compilation
     ) => {
-
+      // console.log(compilation.);
+      const resultFileSystem = getAllFiles('./src');
     })
   }
+}
+
+function getAllFiles (dir) {
+  return fs.readdirSync(dir).reduce((files, file) => {
+      const name = path.join(dir, file);
+      const isDirectory = fs.statSync(name).isDirectory();
+      return isDirectory ? [...files, ...getAllFiles(name)] : [...files, name];
+  }, []);
 }
 
 module.exports = FindUnusedCodePlugin;
